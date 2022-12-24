6e6689607b903b5eae67ed2a5b35f2762fe38ff8
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-24 00:17:44 -0600

    add helper funcs in radix sort

--- sort/radix.js
+++ sort/radix.js
@@ -1,1 +1,18 @@
-export default function radixSort() {}
+const getDigit = (num, i) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+};
+
+const countDigits = (num) => {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+};
+
+const mostDigits = (nums) => {
+  let max = 0;
+  for (let i = 0; i < nums.length; i++) {
+    max = Math.max(max, countDigits(nums[i]));
+  }
+  return max;
+};
+
+export default function radixSort(arr) {}
