f4bf1101a376c2c85f6b852f63c721ee3e9249af
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-24 19:55:56 -0600

    add pop method to SLL

--- list/singly-linked.js
+++ list/singly-linked.js
@@ -25,4 +25,25 @@ export default class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.length === 0) return undefined;
+
+    let current = this.head;
+    let prev = current;
+
+    while (current.next) {
+      prev = current;
+      current = current.next;
+    }
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
