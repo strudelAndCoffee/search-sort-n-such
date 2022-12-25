a43794d2eb909707fb44b45d85a629be5e83ae36
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-25 01:24:15 -0600

    add push method to DLL

---
+++ list/doubly-linked.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+export default class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+
+    return this;
+  }
+}
