b5a747b97355854d0d981b73deee1e3eaf37c9a4
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-24 19:07:49 -0600

    start singly linked list class

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
 // import { linearSearch, binarySearch } from './search/index.js';
-import {
-  bubbleSort,
-  selectioSort,
-  insertionSort,
-  mergeSort,
-  quickSort,
-  radixSort,
-} from './sort/index.js';
-import { randomBetween, shuffle, findMedian } from './helpers/utils.js';
+// import {
+//   bubbleSort,
+//   selectioSort,
+//   insertionSort,
+//   mergeSort,
+//   quickSort,
+//   radixSort,
+// } from './sort/index.js';
+// import { randomBetween, shuffle, findMedian } from './helpers/utils.js';
 
-const arr = [];
-for (let i = 100000; i > 0; i--) arr.push(randomBetween(1, 100000));
-shuffle(arr);
-
-// bubbleSort(arr);
-// selectioSort(arr);
-// insertionSort(arr);
-// const newArr = mergeSort(arr);
-// const newArr = quickSort(arr);
-const newArr = radixSort(arr);
-
-// console.log(newArr);
-console.log(
-  newArr[0],
-  newArr[Math.floor(newArr.length / 2)],
-  newArr[newArr.length - 1]
-);
+// const arr = [];
+// for (let i = 100000; i > 0; i--) arr.push(randomBetween(1, 100000));
+// shuffle(arr);
