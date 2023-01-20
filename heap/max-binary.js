e952c8a4db7ef9bdc384c525cc9a33b4a4b6d9ca
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-01-20 14:43:01 -0600

    add insert method to MBH

--- heap/max-binary.js
+++ heap/max-binary.js
@@ -1,5 +1,25 @@
 export default class MaxBinaryHeap {
   constructor() {
-    this.values = []
+    this.values = [41, 39, 33, 18, 27, 12]
+  }
+
+  insert(val) {
+    this.values.push(val)
+    this.bubble_up()
+  }
+
+  bubble_up() {
+    let index = this.values.length - 1
+    const element = this.values[index]
+
+    while (index > 0) {
+      let parent_index = Math.floor((index - 1) / 2)
+      let parent = this.values[parent_index]
+      if (this.values[index] < this.values[parent_index]) break
+
+      this.values[parent_index] = element
+      this.values[index] = parent
+      index = parent_index
+    }
   }
 }
--- main.js
+++ main.js
@@ -10,4 +10,14 @@
 // import { randomBetween, shuffle, findMedian } from './helpers/utils.js';
 // import { SinglyLinkedList, DoublyLinkedList } from './list/index.js';
 // import { Stack, Queue } from './queue/index.js'
-import { BinarySearchTree } from './tree/index.js'
+// import { BinarySearchTree } from './tree/index.js'
+import { MaxBinaryHeap } from './heap/index.js'
+
+const heap = new MaxBinaryHeap()
+
+console.log(heap)
+heap.insert(55)
+heap.insert(1)
+heap.insert(45)
+heap.insert(111)
+heap.values.forEach((v) => console.log(v))
