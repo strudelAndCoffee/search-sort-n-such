1ed44992a2d05f87b75898cb423860ea09071196
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-01-24 15:53:20 -0600

    add extract max method to MBH

--- heap/max-binary.js
+++ heap/max-binary.js
@@ -22,4 +22,46 @@ export default class MaxBinaryHeap {
       index = parent_index
     }
   }
+
+  bubble_down() {
+    const length = this.values.length
+    const element = this.values[0]
+    let index = 0
+
+    while (true) {
+      let left_i = 2 * index + 1
+      let right_i = 2 * index + 2
+      let left_child, right_child
+      let swap = null
+
+      if (left_i < length) {
+        left_child = this.values[left_i]
+        if (left_child > element) swap = left_i
+      }
+      if (right_i < length) {
+        right_child = this.values[right_i]
+        if (
+          (swapped === null && right_child > element) ||
+          (swapped !== null && right_child > left_child)
+        )
+          swap = right_i
+      }
+      if (swap === null) break
+
+      this.values[index] = this.values[swap]
+      this.values[swap] = element
+      index = swap
+    }
+  }
+
+  extract_max() {
+    const max = this.values[0]
+    const end = this.values.pop()
+    if (this.values.length > 0) {
+      this.values[0] = end
+      this.bubble_down()
+    }
+
+    return max
+  }
 }
