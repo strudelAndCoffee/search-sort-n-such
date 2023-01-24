9a53ea0bf88d89cfd8dca1a58bb5d7d06fd6c975
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-01-24 17:11:32 -0600

    edit comments for heaps

--- heap/index.js
+++ heap/index.js
@@ -1,4 +1,9 @@
-// Big O (time): all heaps have O(log N)
+// Binary Heaps are ideal for adding/removing items from a list with automatic ordering and removing the root item
+// Not ideal for searching
+
+// Big O (time):
+// all heaps have O(log N) for insert/removal
+// all heaps have O(N) for searching
 
 import MaxBinaryHeap from './max-binary.js'
 import MinBinaryHeap from './min-binary.js'
