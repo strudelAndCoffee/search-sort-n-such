84c2ce09719ad936a93bf628f24fae6c7770f707
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-01-27 14:38:34 -0600

    finish course

--- algorithms/dijkstra-shortest-path.js
+++
@@ -1,4 +0,0 @@
-import { PriorityQueue } from '../heap/index.js'
-import { WeightedUndirectedGraph } from '../graph/index.js'
-
-export default function dijkstraShortestPath() {}
--- main.js
+++ main.js
@@ -13,23 +13,4 @@
 // import { BinarySearchTree } from './tree/index.js'
 // import { MaxBinaryHeap } from './heap/index.js'
 // import HashTable from './hash_table/hash-table.js'
-import { WeightedUndirectedGraph } from './graph/index.js'
-
-const graph = new WeightedUndirectedGraph()
-graph.addVertex('A')
-graph.addVertex('B')
-graph.addVertex('C')
-graph.addVertex('D')
-graph.addVertex('E')
-graph.addVertex('F')
-
-graph.addEdge('A', 'B', 4)
-graph.addEdge('A', 'C', 2)
-graph.addEdge('B', 'E', 3)
-graph.addEdge('C', 'D', 2)
-graph.addEdge('C', 'F', 4)
-graph.addEdge('D', 'E', 3)
-graph.addEdge('D', 'F', 1)
-graph.addEdge('E', 'F', 1)
-
-console.log(graph.dijkstraShortestPath('A', 'E'))
+// import { WeightedUndirectedGraph } from './graph/index.js'
